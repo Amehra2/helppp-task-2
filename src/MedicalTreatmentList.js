d39c04aaef6f420acc7f135079f473a8a5df927d
Amehra2 <akashmehra2001@hotmail.com>
2021-11-12 21:23:27 +0000

    2 files modified

--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -4,7 +4,7 @@ import MedicalTreatment from './MedicalTreatment';
 function MedicalTreatmentList(props){
   const MedicalTreatmentListJ = props.json;
   const MedicalTreatmentList = JSON.parse(MedicalTreatmentListJ);
-  const ListMedicalTreatmentList = MedicalTreatmentList.map((product)=>
+  const ListMedicalTreatmentList = MedicalTreatmentList.map((MedicalTreatment)=>
   <li>
     <MedicalTreatment treatId= {MedicalTreatment.treatId} treatCourseId= {MedicalTreatment.treatCourseId} type={MedicalTreatment.type} category={MedicalTreatment.category} name={MedicalTreatment.name} startDate={MedicalTreatment.startDate} />
   </li>);
