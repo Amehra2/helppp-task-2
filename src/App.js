2015375523f4d36b65a857d1b6bd2b83ae64ff16
Amehra2 <akashmehra2001@hotmail.com>
2021-11-13 05:41:18 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     '{"treatId": "LHM403", "treatCourseId":"40330", "type": "long term", "category": "Lower Hernia", "name": "Daniel Westford", "startDate": "14-02-2019"} ,' +
     '{"treatId": "LHM404", "treatCourseId":"40440", "type": "long term", "category": "Epilepsy", "name": "Peter Mcgorge", "startDate": "04-05-2017"} ,' +
     '{"treatId": "LHM405", "treatCourseId":"40550", "type": "long term", "category": "Heart patient", "name": "Frank Alexander", "startDate": "24-07-2016"} ,' +
-    '{"treatId": "LHM406", "treatCourseId":"406440", "type": "short term", "category": "Hamstring injury", "name": "Omar Adnan", "startDate": "13-10-2021"}]';
+    '{"treatId": "LHM406", "treatCourseId":"40644", "type": "short term", "category": "Hamstring injury", "name": "Omar Adnan", "startDate": "13-10-2021"}]';
 
 return (
     <div>
