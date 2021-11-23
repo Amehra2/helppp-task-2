7952917720bc9d2b8c30b02f1a56927523faa1ad
Amehra2 <akashmehra2001@hotmail.com>
2021-11-23 14:43:52 +0000

    1 file modified

--- src/MedicalTreatmentList.js
+++ src/MedicalTreatmentList.js
@@ -5,27 +5,14 @@ function MedicalTreatmentList(props){
   const medicalTreatmentListJ  = props.json;
   const medicaltreatmentlist = JSON.parse(medicalTreatmentListJ);
 
-
-
-
 /*
 
 Prior to the .map function i have applied .filter() which displays medicaltreatmentlist if the category is equal to Sciatica
 
 */
-
-
   const ListMedicalTreatmentList = medicaltreatmentlist.filter(medicalTreatment => medicalTreatment.category === 'Sciatica').map((medicalTreatment)=>
 
-
-
-
-
-
-
-
-
-  <li>
+<li>
     <MedicalTreatment treatId={medicalTreatment.treatId} treatCourseId= {medicalTreatment.treatCourseId} type={medicalTreatment.type} category={medicalTreatment.category} name={medicalTreatment.name} startDate={medicalTreatment.startDate} />
   </li>);
   return(
